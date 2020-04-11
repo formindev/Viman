0051cf6f20d176a43b21197a1dabb1634be3c712
formindev <formindeve@gmail.com>
2020-04-12 00:08:19 +0400

    Added view, templates

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
-    item_type = models.ForeignKey("Item_type", on_delete=models.SET_NULL, null=True)
+    item_type = models.ForeignKey(
+        "Item_type", 
+        on_delete=models.SET_NULL, 
+        null=True,
+    )
     path = models.FileField(upload_to='video/')
     cover = models.ImageField()
 
--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.template import loader
+
+from .models import Item
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    item_list = Item.objects.all()
+    template = loader.get_template('catalog/index.html')
+    context = {
+        'item_list': item_list,
+    }
+    return HttpResponse(template.render(context, request))
