9484f7324e646dfd8cf58781ba3a6bb85577368b
formindev <formindeve@gmail.com>
2020-04-11 23:25:12 +0400

    Updated catalog model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    item_type = models.ForeignKey("Item_type", on_delete=models.SET_NULL, null=True)
+    path = models.FileField(upload_to='video/')
+    cover = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+class Item_type(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
